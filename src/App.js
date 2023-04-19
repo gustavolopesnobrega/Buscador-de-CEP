edc2dfe4a7371b2108125e63efcb83a5497b967a
gustavolopesnobrega <gustavo.lopes.nobrega@gmail.com>
2023-04-18 22:31:06 -0300

    feat: cep validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './style.css'
-
+import { Component } from 'react';
 import api from './services/api'
 
 function App() {
 
   const [input, setInput] = useState('')
   const [cep, setCep] = useState({});
+  
+  const backgroundJp = {
+    backgroundImage: `url("https://solutudo-cdn.s3-sa-east-1.amazonaws.com/prod/adv_files/57f6703b-95e0-49db-8455-5e61ac1f1344/1f22e18f-1764-4edc-b59d-546ac1716111.jpg")` 
+  }
+  const backgroundGeneral = {
+    background: 'linear-gradient(#121212, #212b46)'
+  }
+
 
   async function handleSearch() {
     if (input === "" ) {
@@ -19,17 +27,24 @@ function App() {
       const response = await api.get(`${input}/json`);
       setCep(response.data)
       setInput("")
+      console.log(response.data.erro)
+      if(response.data.erro == true) {
+        alert("Por favor digite um cep valido")
+      }
 
     } catch {
+      
       alert("erro ao buscar cep");
+      
       setInput("")
     }
-
+    
+ 
   }
 
   return (
-    <div className="container">
-      <h1 className="title">Buscador de CEP</h1>
+    <div className="container" >
+      <h1 >Buscador de CEP</h1>
 
       <div className="containerInput">
         <input type="text" placeholder="Digite seu cep" value={input} 
